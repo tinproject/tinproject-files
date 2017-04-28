25ebc05dc0cebbd6f54b3aa3e8a692262be4908e
agustin <agustin.herranz@gmail.com>
2017-04-28 13:43:06 +0200

    Add installer scripts

---
+++ scripts/installer-hashicorp.sh
@@ -0,0 +1,158 @@
+#!/usr/bin/env bash
+
+## Download and install Hashicorp applications
+#  Usage = $0 <product> <version> \n
+#   - product = Hasicorp product, e.g.: packer
+#   - version = build version, e.g.: 0.12.
+
+USAGE="Usage = $0 <product> <version>"
+
+## Issues & not implemented things:
+#  - Other platforms different from linux
+#  - Get latest by default
+#  - Has to have write permissions on the parent of $BASE_FOLDER
+#  - Treat Vagrant as a special option, downloads a -deb not a .zip
+
+## Rerurn codes
+#  0 - Product installed
+#  1 - Calling with bad parameters
+#  2 - Problems verifying files
+#  3 - Lack of prerequisites
+
+PRODUCT="non_valid_product"
+VERSION="non_valid_version"
+OS="linux"
+ARCH="amd64"
+
+BASE_URL="https://releases.hashicorp.com"
+GPG_LONG_ID="51852D87348FFC4C"
+GPG_KEY_FINGERPRINT="91A6E7F85D05C65630BEF18951852D87348FFC4C"
+GPG_KEYSERVER="hkp://keys.gnupg.net/"
+GPG_KEYSERVER="hkp://keyserver.ubuntu.com/"
+
+JQ_NULL="null"
+
+function exit_if_error() {
+    if [ -n ${S2} ]; then
+        echo $2
+    fi
+    if [ -n ${TEMP_FOLDER} -a -d ${TEMP_FOLDER} ]; then
+        #echo "Removing temp folder."
+        rm -rf ${TEMP_FOLDER}
+    fi
+    exit $1
+}
+
+JQ=$(which jq)
+CURL=$(which curl)
+WGET=$(which wget)
+
+if [ -z "$JQ" -o -z "$CURL" -o -z "$WGET" ]; then
+  exit_if_error 3 "This script requires jq, curl, & wget"
+fi
+
+# test parameters
+if [ -z $1 ]; then
+    echo ${USAGE}
+    exit_if_error 1 "Product not specified"
+fi
+PRODUCT="${1}"
+if [ -z $2 ]; then
+    echo ${USAGE}
+    exit_if_error 1 "Version not specified"
+fi
+VERSION="${2}"
+
+BASE_FOLDER="/opt/hashi"
+INSTALL_FOLDER="${BASE_FOLDER}/${PRODUCT}"
+
+echo "* Searching for ${PRODUCT} version ${VERSION}..."
+
+set -e
+# Download data from Hashicorp API. See: https://www.hashicorp.com/blog/hashicorp-and-fastly.html
+hashi_json=$(curl -s ${BASE_URL}/index.json | jq .)
+
+product_json=$(echo ${hashi_json} | jq -r --arg product ${PRODUCT} '.[$product].versions')
+if [[ "${product_json}" == "${JQ_NULL}" ]]; then
+    echo "Could not locate the Hashicorp product ${PRODUCT}."
+    echo "Must be one of this:"
+    vars=( $(echo ${hashi_json} | jq -r 'keys | @sh' ) )
+    for f in "${vars[@]}"; do echo "$f" ; done
+    exit 1
+fi
+
+version_json=$(echo ${product_json} | jq -r --arg version ${VERSION} '.[$version]')
+if [[ "${version_json}" == "${JQ_NULL}" ]]; then
+    echo "Could not locate version ${VERSION} for product ${PRODUCT}."
+    echo "Must be one of this:"
+    vars=( $(echo ${product_json} | jq -r 'keys | @sh' ) )
+    for f in "${vars[@]}"; do echo "$f" ; done
+    exit 1
+fi
+
+build_json=$(echo ${version_json} | jq -r --arg os ${OS} '.builds | .[] | select(.os == $os)')
+if [[ "${build_json}" == "" ]]; then
+    echo "Could not find OS ${OS} for product ${PRODUCT} ${VERSION}."
+    echo "Must be one of this:"
+    vars=( $(echo ${version_json} | jq -r '[.builds | .[].os]  | unique | @sh' ) )
+    for f in "${vars[@]}"; do echo "$f" ; done
+    exit 1
+fi
+
+release_json=$(echo ${build_json} | jq -r --arg arch ${ARCH} 'select(.arch == $arch)')
+if [[ "${release_json}" == "" ]]; then
+    echo "Could not find arch ${ARCH} for product ${PRODUCT} ${VERSION} for ${OS}."
+    echo "Must be one of this:"
+    vars=( $(echo ${build_json} | jq -r '[.arch] | unique | @sh') )
+    for f in "${vars[@]}"; do echo "$f" ; done
+    exit 1
+fi
+
+
+file_url=$(echo ${release_json} | jq -r '.url')
+file_name=$(echo ${release_json} | jq -r '.filename')
+shasums=$(echo ${version_json} | jq -r '.shasums')
+shasums_signature=$(echo ${version_json} | jq -r '.shasums_signature')
+
+TEMP_FOLDER=$(mktemp -d)
+
+export GNUPGHOME="${TEMP_FOLDER}"
+
+echo "* Downloading files..."
+
+wget -q -O "${TEMP_FOLDER}/${file_name}" --https-only "${file_url}"
+echo "${BASE_URL}/${PRODUCT}/${VERSION}/${shasums}"
+wget -q -O "${TEMP_FOLDER}/${shasums}" --https-only "${BASE_URL}/${PRODUCT}/${VERSION}/${shasums}"
+wget -q -O "${TEMP_FOLDER}/${shasums_signature}" --https-only "${BASE_URL}/${PRODUCT}/${VERSION}/${shasums_signature}"
+
+echo "* Obtaining gpg keys..."
+set +e
+gpg -q --keyserver ${GPG_KEYSERVER} --recv-keys ${GPG_KEY_FINGERPRINT} > /dev/null
+
+echo "* Veryifying signatures..."
+gpg -q --batch --trusted-key "0x${GPG_LONG_ID}" --verify "${TEMP_FOLDER}/${shasums_signature}" "${TEMP_FOLDER}/${shasums}" 2> /dev/null
+rv=$?
+if [ ${rv} != 0 ]; then
+    exit_if_error 2 "Error verifying shasum signature."
+fi
+
+echo "* Veryifying checksums..."
+cat "${TEMP_FOLDER}/${shasums}" | grep "${file_name}" > "${TEMP_FOLDER}/shasum"
+(cd ${TEMP_FOLDER} && sha256sum --status --check "${TEMP_FOLDER}/shasum")
+rv=$?
+if [ ${rv} != 0 ]; then
+    exit_if_error 2 "Error verifying file checksums."
+fi
+
+echo "* Installing ${PRODUCT} ${VERSION}..."
+if [ ! -d INSTALL_FOLDER ]; then
+    mkdir -p ${INSTALL_FOLDER}
+fi
+
+unzip -q -o -d "${INSTALL_FOLDER}" "${TEMP_FOLDER}/${file_name}"
+
+echo "* Installation finished: ${PRODUCT} ${VERSION} installed on ${INSTALL_FOLDER}"
+echo "To run ${PRODUCT} you need to add to the path, or create a link to the executable:"
+echo "- Create a link: \$sudo ln -s ${INSTALL_FOLDER}/${PRODUCT} /usr/bin/${PRODUCT}"
+echo "- Add install folder to \$PATH on ~/.bashrc: PATH${INSTALL_FOLDER}:\$PATH"
+exit_if_error 0
---
+++ scripts/installer-pycharm.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+## Issues & not implemented things:
+#  - Other platforms different from linux
+#  - Get a different release, only latest
+#  - Has to have write permissions on the parent of $INSTALL_FOLDER
+
+## Rerurn codes
+#  0 - Product installed
+#  1 - Cancel by user
+#  2 - Problems downloading or verifying files
+#  3 - Lack of prerequisites
+
+JQ=$(which jq)
+CURL=$(which curl)
+WGET=$(which wget)
+
+if [ -z "$JQ" -o -z "$CURL" -o -z "$WGET" ]; then
+  echo "This script requires jq, curl, & wget"
+  exit 3
+fi
+
+set -e
+
+PRODUCT_NAME="PyCharm"
+PRODUCT="${PRODUCT_NAME,,}"
+PRODUCT_CODE="PCP"  # PCP -> PyCharm Professional
+RELEASE_TYPE="release"  # release | eap
+RELEASE_LATEST="true"  # true | false -> Only the latest release or all
+PLATFORM="linux"
+
+TMP_FOLDER="/tmp/installer/jetbrains"
+DOWNLOAD_FOLDER="${TMP_FOLDER}/${PRODUCT}"
+INSTALL_FOLDER="/opt/${PRODUCT}"
+
+# query Jetbrains data services
+echo "Downloading information for ${PRODUCT} latest release"
+
+json=$(curl -s "https://data.services.jetbrains.com/products/releases?code=${PRODUCT_CODE}&latest=${RELEASE_LATEST}&type=${RELEASE_TYPE}")
+
+release_obj=$(echo ${json} | jq -r .[][])
+
+# Extract data from retrieved json
+declare -r rel_build=$(echo ${release_obj} | jq -r '.build')
+declare -r rel_version=$(echo ${release_obj} | jq -r '.version')
+declare -r rel_major=$(echo ${release_obj} | jq -r '.majorVersion')
+declare -r rel_date=$(echo ${release_obj} | jq -r '.date')
+declare -r rel_type=$(echo ${release_obj} | jq -r '.type')
+declare -r rel_file_url=$(echo ${release_obj} | jq -r ".downloads.${PLATFORM}.link")
+declare -r rel_shasum_url=$(echo ${release_obj} | jq -r ".downloads.${PLATFORM}.checksumLink")
+declare -r rel_file_size=$(echo ${release_obj} | jq -r ".downloads.${PLATFORM}.size")
+
+declare -r rel_file="${rel_file_url##*/}"
+declare -r rel_shasum="${rel_shasum_url##*/}"
+
+# Check if product is already installed
+if [ -d "${INSTALL_FOLDER}" ]; then
+    cur_build=$(cat ${INSTALL_FOLDER}/build.txt)
+    cur_build="${cur_build##PY-}"
+    echo "Currently installed build: ${cur_build} on ${INSTALL_FOLDER}."
+    if [[ "${cur_build}" < "${rel_build}" ]]; then
+        echo "New ${PRODUCT_NAME} version available: ${rel_version}, build: ${rel_build}"
+        # Ask if want to install the new version, only if there is an already installed version
+        while true; do
+            read -p "Do you want to install the new version? [Y/n]"
+            case $REPLY in
+                [Yy]* ) break;;
+                "" ) break;;
+                [Nn]* ) exit 1;;
+            esac
+        done
+    else
+        echo "Latest version of ${PRODUCT_NAME} installed."
+        exit 0
+    fi
+else
+    mkdir -p "${INSTALL_FOLDER}"
+fi
+
+set +e
+
+function exit_on_file_error() {
+    #rm ${DOWNLOAD_FOLDER}/${rel_file}
+    exit 2
+}
+
+echo "Downloading ${PRODUCT_NAME} ${rel_version}"
+# Create download folder, this folders wont be removed for cache
+mkdir -p ${DOWNLOAD_FOLDER}
+
+wget -nc -q -O "${DOWNLOAD_FOLDER}/${rel_file}" --https-only "${rel_file_url}"
+if [[ "$(wc -c < ${DOWNLOAD_FOLDER}/${rel_file})" != "${rel_file_size}" ]]; then
+    echo "Error: downloaded file don't have the correct size, exiting."
+    exit_on_file_error
+fi
+
+wget -q -O "${DOWNLOAD_FOLDER}/${rel_shasum}" --https-only "${rel_shasum_url}"
+
+echo "- Verifying files..."
+( cd ${DOWNLOAD_FOLDER} && sha256sum --status -c "${DOWNLOAD_FOLDER}/${rel_shasum}" )
+rv=$?
+if [ ${rv} != 0 ]; then
+    echo "Error: the file don't have the correct checksum, exiting."
+    exit_on_file_error
+fi
+
+set -e
+
+echo "Extracting ${PRODUCT_NAME} ${rel_version}"
+tar  -C "${DOWNLOAD_FOLDER}" -xzf "${DOWNLOAD_FOLDER}/${rel_file}"
+if [ ! -d "${DOWNLOAD_FOLDER}/${PRODUCT}-${rel_version}" ]; then
+    echo "Error: fail at product extraction."
+    exit_on_file_error
+fi
+
+echo "Installing ${PRODUCT_NAME} ${rel_version}"
+# rename current install folder
+mv "${INSTALL_FOLDER}" "${INSTALL_FOLDER}.old"
+# install new version
+mv "${DOWNLOAD_FOLDER}/${PRODUCT}-${rel_version}" "${INSTALL_FOLDER}"
+#remove old version
+rm -rf "${INSTALL_FOLDER}.old"
+
+exit 0
